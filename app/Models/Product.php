7c77ed4d013d7b4a79f618b89b79016545c8143e
HooiErn <d200217c.sc.edu.my>
2021-11-22 22:40:54 +0800

    Update add method

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,10 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
-     protected $fillable=['name','description','price','image','quantity','category' ]; 
-     public function product(){
+    protected $fillable=['name','description','price','image','quantity','CategoryID'];
+    public function product(){
         return $this->belongsTo('App\Models\Category');
-    
-
-}
+    }
 }
