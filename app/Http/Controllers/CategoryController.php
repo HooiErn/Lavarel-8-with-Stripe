ef24793d7d588628fa2ae2f90e35613eb5a20b54
HooiErn <d200217c.sc.edu.my>
2021-12-03 12:19:48 +0800

    edit function

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -14,6 +14,7 @@ class CategoryController extends Controller
         $addCategory=Category::create([
             'name'=>$r->categoryName,
         ]);
+        Session::flash('success',"Category create successfully!");
         Return view('addCategory');
     }
     public function view(){
--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use DB;
 use App\Models\Product;
 use Session;
+use App\Models\Category;
 
 class ProductController extends Controller
 {
@@ -34,5 +35,37 @@ class ProductController extends Controller
         ->get();
 
         Return view('showProduct')->with('products',$viewProduct);
+    }
+    public function delete($id){
+        $deleteProduct=Product::find($id);
+        $deleteProduct->delete();
+        Session::flash('success',"Product was delete successfully!");
+        Return redirect()->route('showProduct');
+    }
+    public function edit($id){
+       $products=Product::all()->where('id',$id);
+       Return view('editProduct')->with('products',$products)
+                                 ->with('categoryID',Category::all());
+    
+    }
+    public function update(){
+        $r=request();
+        $products =Product::find($r->productID);
+
+        if($r->file('productImage')!=''){
+            $image=$r->file('productImage');        
+            $image->move('images',$image->getClientOriginalName());                   
+            $imageName=$image->getClientOriginalName(); 
+            $products->image=$imageName;
+            }    
+        
+        $products->name=$r->productName;
+        $products->description=$r->productDescription;
+        $products->price=$r->productPrice;
+        $products->quantity=$r->productQuantity;
+        $products->CategoryID=$r->CategoryID;
+        $products->save();
+
+        Return redirect()->route('showProduct');
     }
 }
